0ebbdbca73f2b1652b38fcc802a0cb4478b5c542
rhung192 <110218331+rhung192@users.noreply.github.com>
2026-04-24 23:33:06 -0400

    Small login change

--- tranner/src/main/java/com/tranner/model/gui/mainMenu.java
+++ tranner/src/main/java/com/tranner/model/gui/mainMenu.java
@@ -4,10 +4,26 @@ import javax.swing.*;
 
 public class mainMenu extends JPanel{
     private JButton loginButton, registerButton;
+    private JTextField loginUsernameField, loginPasswordField;
+    private JLabel loginlbl, passwordlbl, tempLogo;
+
     public mainMenu(){
+        //this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        this.tempLogo = new JLabel("Tranner Logo Here");
+
+        this.loginlbl = new JLabel("Username:");
+        this.passwordlbl = new JLabel("Password:");
+        this.loginUsernameField = new JTextField(10);
+        this.loginPasswordField = new JTextField(10);
+
         this.loginButton = new JButton("Login");
         this.registerButton = new JButton("Register");
 
+        this.add(tempLogo);
+        this.add(loginlbl);
+        this.add(loginUsernameField);
+        this.add(passwordlbl);
+        this.add(loginPasswordField);
         this.add(loginButton);
         this.add(registerButton);
     }
